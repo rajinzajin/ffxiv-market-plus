d9463b60cd935c6d9ed4139091eb6296b6d7d9b7
rajinzajin <dorgclabs@gmail.com>
2023-04-14 14:48:38 +0800

    improved search ui

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,11 +4,7 @@
 
 	let searchResultVisible = "invisible";
 	let searchResult = [];
-
-	onMount(async () => {
-		// var res = await axios("/api/data-centers");
-		// console.log(res.data);
-	});
+	let searchText = "";
 
 	async function onSearch(e) {
 		var item_name = e.target.value;
@@ -20,6 +16,26 @@
 		searchResultVisible =
 			searchResult.length > 0 && item_name !== "" ? "visible" : "invisible";
 	}
+
+	function onSelectItemSearch(selected_item) {
+		searchResultVisible = "invisible";
+		searchText = selected_item;
+	}
+
+	function handleClick(event) {
+		const search_result_element = document.getElementById("search_result");
+		const search_input_element = document.getElementById("search_input");
+		if (
+			!search_result_element.contains(event.target) &&
+			!search_input_element.contains(event.target)
+		) {
+			searchResultVisible = "invisible";
+		}
+	}
+
+	onMount(() => {
+		document.addEventListener("click", handleClick);
+	});
 </script>
 
 <div class="px-6 py-8 bg-secondary">
@@ -31,19 +47,28 @@
 			<input
 				autocomplete="off"
 				type="text"
-				id="first_name"
+				id="search_input"
 				class="bg-primary w-full outline-none focus:outline-none mt-4 text-gray-400 text-sm rounded-lg block p-2.5"
 				placeholder="Search item"
 				on:input={onSearch}
+				on:focus={onSearch}
+				bind:value={searchText}
 			/>
 			<div class="relative">
 				<div
+					id="search_result"
 					class="{searchResultVisible} bg-item absolute max-h-80 rounded-2xl overflow-y-auto mt-2 pl-2 w-full z-50 text-white"
 				>
 					<ul>
 						{#each searchResult as item}
-							<li class="h-auto">
-								<div class="flex m-auto hover:bg-primary py-2">
+							<li class="h-auto cursor-pointer">
+								<div
+									class="flex m-auto hover:bg-primary py-2"
+									on:keydown={() => {}}
+									on:click={() => {
+										onSelectItemSearch(item.en);
+									}}
+								>
 									<img
 										src="https://rajinzajin.github.io/ffxiv-assets/icon2x/{item._id}.png"
 										alt="Flowbite Logo"
