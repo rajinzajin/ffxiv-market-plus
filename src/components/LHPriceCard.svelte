1035e67304f7cdda119ea37a9009efdd62fdb852
rajinzajin <dorgclabs@gmail.com>
2023-04-15 10:18:58 +0800

    added LHPriceCard component

---
+++ src/components/LHPriceCard.svelte
@@ -0,0 +1,22 @@
+<script>
+    export let item
+    export let title
+    export let color
+</script>
+
+<div class="h-100 mt-3 p-4 rounded-lg bg-primary">
+    <h1 class="text-gray-300 font-body">{title}</h1>
+    <h1 class="text-{color} font-body font-black text-xl mt-2">
+        {item != null ? item.pricePerUnit : "-"}
+    </h1>
+    {#if item != null}
+        <div class="flex items-center">
+            <span class="flex-auto text-summary font-body font-[0]">
+                {item.quantity} Unit{item.quantity > 1
+                    ? "s"
+                    : ""}
+            </span>
+            <span class="flex-auto font-body font-semibold text-lg text-gray-400 text-right">{item.worldName}</span>
+        </div>
+    {/if}
+</div>
--- src/routes/market/[item_id]/+page.svelte
+++ src/routes/market/[item_id]/+page.svelte
@@ -1,4 +1,5 @@
 <script>
+	import LhPriceCard from "../../../components/LHPriceCard.svelte";
 	import { getItemImageUrl } from "../../../utils/item_utils.js";
 
 	export let data;
@@ -17,55 +18,23 @@
 		>
 			<h1 class="text-white font-display text-2xl font-bold">Lowest Price</h1>
 
-			<div class="h-100 mt-3 p-4 rounded-lg bg-primary flex">
-				<div class="">
-					<h1 class="text-gray-300 font-body">Normal Quality</h1>
-					<h1 class="text-money font-body font-black text-xl mt-2">
-						{data.nqLowest != null ? data.nqLowest.pricePerUnit : "-"}
-					</h1>
-                    {#if data.nqLowest != null}
-					<h4 class="text-summary font-body font-[0]">{data.nqLowest.quantity} Unit{data.nqLowest.quantity > 1 ? "s" : ""}</h4>
-                    {/if}
-				</div>
-			</div>
-			<div class="h-100 mt-3 p-4 rounded-lg bg-primary flex">
-				<div class="">
-					<h1 class="text-gray-300 font-body">High Quality</h1>
-					<h1 class="text-money font-body font-black text-xl mt-2">
-						{data.hqLowest != null ? data.hqLowest.pricePerUnit : "-"}
-					</h1>
-                    {#if data.hqLowest != null}
-					<h3 class="text-summary font-body font-[0]">{data.hqLowest.quantity} Unit{data.hqLowest.quantity > 1 ? "s" : ""}</h3>
-                    {/if}
-				</div>
-			</div>
+			<LhPriceCard color="money" title="Normal Quality" item={data.nqLowest} />
+			<LhPriceCard color="money" title="High Quality" item={data.hqLowest} />
 		</div>
 		<div
 			class="h-100 p-5 w-96 max-w-full items-center justify-center rounded-2xl bg-item mt-6"
 		>
 			<h1 class="text-white font-display text-2xl font-bold">Highest Price</h1>
-			<div class="h-100 mt-3 p-4 rounded-lg bg-primary flex">
-				<div class="">
-					<h1 class="text-gray-300 font-body">Normal Quality</h1>
-					<h1 class="text-money2 font-body font-black text-xl mt-2">
-						{data.nqHighest != null ? data.nqHighest.pricePerUnit : "-"}
-					</h1>
-                    {#if data.nqHighest != null}
-					<h3 class="text-summary font-body font-[0]">{data.nqHighest.quantity} Unit{data.nqHighest.quantity > 1 ? "s" : ""}</h3>
-                    {/if}
-				</div>
-			</div>
-			<div class="h-100 mt-3 p-4 rounded-lg bg-primary flex">
-				<div class="">
-					<h1 class="text-gray-300 font-body">High Quality</h1>
-					<h1 class="text-money2 font-body font-black text-xl mt-2">
-						{data.hqHighest != null ? data.hqHighest.pricePerUnit : "-"}
-					</h1>
-                    {#if data.hqHighest != null}
-					<h3 class="text-summary font-body font-[0]">{data.hqHighest.quantity} Unit{data.hqHighest.quantity > 1 ? "s" : ""}</h3>
-                    {/if}
-				</div>
-			</div>
+			<LhPriceCard
+				color="money2"
+				title="Normal Quality"
+				item={data.nqHighest}
+			/>
+			<LhPriceCard
+				color="money2"
+				title="High Quality"
+				item={data.hqHighest}
+			/>
 		</div>
 	</div>
 </div>
