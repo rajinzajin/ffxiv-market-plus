d2aa5766d3ee671132032dae16db27b7563272f2
rajinzajin <dorgclabs@gmail.com>
2023-04-16 22:21:59 +0800

    MarketEvents improvements

--- src/components/MarketEvents.svelte
+++ src/components/MarketEvents.svelte
@@ -1,26 +1,33 @@
 <script>
-	// import { serialize, deserialize } from "bson";
 	import { onMount } from "svelte";
-	import { json } from "@sveltejs/kit";
-	//TODO need fix bson top level error
 	import { BSON } from "../../node_modules/bson/lib/bson.mjs";
-	
+	import { get } from "svelte/store";
+	import { world_store } from "../stores/dc_world_stores.js";
+
 	let socket;
+	let lastWorldList = [];
 	onMount(() => {
 		const addr = "wss://universalis.app/api/ws";
 
 		socket = new WebSocket(addr);
 		socket.onopen = function () {
-			console.log("Opened");
-			socket.send(
-				BSON.serialize({ event: "subscribe", channel: "sales/add" })
-			);
+			console.log("WebSocket Opened");
+			world_store.subscribe((new_world_list) => {
+				lastWorldList.forEach((world) => {
+					socket.send(BSON.serialize({ event: "unsubscribe", channel: `sales/add{world=${world.id}}` }));
+				});
+				new_world_list.forEach((world) => {
+					socket.send(BSON.serialize({ event: "subscribe", channel: `sales/add{world=${world.id}}` }));
+				});
+				lastWorldList = new_world_list
+			});
 		};
 		socket.onmessage = function (event) {
 			var reader = new FileReader();
 			reader.onload = function () {
 				var uint8Array = new Uint8Array(this.result);
-				console.log(BSON.deserialize(uint8Array));
+				var bsonData = BSON.deserialize(uint8Array);
+				// console.log(bsonData.world);
 			};
 			reader.readAsArrayBuffer(event.data);
 		};
