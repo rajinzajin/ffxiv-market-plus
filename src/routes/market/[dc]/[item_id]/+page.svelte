01651e6baa449f8c0450ac66772df07ace262602
rajinzajin <dorgclabs@gmail.com>
2023-04-16 15:55:29 +0800

    added item search bar on item detail page

--- src/routes/market/[dc]/[item_id]/+page.svelte
+++ src/routes/market/[dc]/[item_id]/+page.svelte
@@ -1,12 +1,19 @@
 <script>
 	import LhPriceCard from "../../../../components/LHPriceCard.svelte";
 	import { getItemImageUrl } from "../../../../utils/item_utils.js";
-
+	import ItemSearchBar from "../../../../components/ItemSearchBar.svelte";
 	export let data;
-	let { item_detail } = data
 </script>
 
 <div>
+	<div
+		class="h-100 py-2 mb-12 px-5 w-96 max-w-full items-center justify-center rounded-2xl bg-item"
+	>
+		<div class="w-full mb-4">
+			<ItemSearchBar />
+		</div>
+	</div>
+
 	<div class="flex">
 		<img class="h-20" src={getItemImageUrl(data.item_id)} alt={data.name} />
 		<div class="ml-5 font-display">
@@ -14,10 +21,9 @@
 				{data.name}
 			</h1>
 			<h1 class="text-gray-400 text-lg font-black my-auto">
-				{item_detail["9"]}
+				{data.item_detail["9"]}
 			</h1>
 		</div>
-		
 	</div>
 	<div class="mt-6">
 		<div
@@ -25,8 +31,16 @@
 		>
 			<h1 class="text-white font-display text-2xl font-bold">Lowest Price</h1>
 
-			<LhPriceCard color="text-money" title="Normal Quality" item={data.nqLowest} />
-			<LhPriceCard color="text-money" title="High Quality" item={data.hqLowest} />
+			<LhPriceCard
+				color="text-money"
+				title="Normal Quality"
+				item={data.nqLowest}
+			/>
+			<LhPriceCard
+				color="text-money"
+				title="High Quality"
+				item={data.hqLowest}
+			/>
 		</div>
 		<div
 			class="h-100 p-5 w-96 max-w-full items-center justify-center rounded-2xl bg-item mt-6"
