6a58f43c665e9a10995d7c826fdffe535d99f5c3
rajinzajin <dorgclabs@gmail.com>
2023-04-16 15:25:16 +0800

    cache for fast page loading

--- src/routes/+page.server.js
+++ src/routes/+page.server.js
@@ -1,8 +1,16 @@
 import axios from 'axios';
 
+let cachedDataCenters;
 /** @type {import('./$types').PageServerLoad} */
 export async function load() {
+	if(cachedDataCenters != null){
+		return{
+			"data_centers" : cachedDataCenters
+		}
+	}
+
 	var res = await axios("https://universalis.app/api/v2/data-centers")
+	cachedDataCenters = res.data
     return {
 		"data_centers" : res.data,
 	};
