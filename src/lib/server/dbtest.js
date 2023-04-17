f4f0414d9537c64f66ae1d7fb328bbb4a31c07b8
rajinzajin <dorgclabs@gmail.com>
2023-04-17 10:05:00 +0800

    test

--- src/lib/server/dbtest.js
+++ src/lib/server/dbtest.js
@@ -1,6 +1,7 @@
 import sqlite3 from "better-sqlite3"
+import { DB_PATH } from '$env/static/private';
 
-const db = sqlite3("ffxiv_database.sqlite3")
+const db = sqlite3(DB_PATH)
 
 export function getItem(){
     var row = db.prepare('SELECT * FROM item WHERE id = ?').get(1)
