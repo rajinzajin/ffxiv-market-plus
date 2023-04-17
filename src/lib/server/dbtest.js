ae8ad7f154d496dbdbb558f4e64548d2a26fa142
rajinzajin <dorgclabs@gmail.com>
2023-04-17 10:25:38 +0800

    Update dbtest.js

--- src/lib/server/dbtest.js
+++ src/lib/server/dbtest.js
@@ -1,9 +1,17 @@
-import sqlite3 from "better-sqlite3"
-import { DB_PATH } from '$env/static/private';
+import sqlite3 from "better-sqlite3";
+import { DB_PATH } from "$env/static/private";
 
-const db = sqlite3("fonts/ffxiv_database.sqlite3")
+import { fileURLToPath } from "url";
+import { dirname } from "path";
 
-export function getItem(){
-    var row = db.prepare('SELECT * FROM item WHERE id = ?').get(1)
-    console.log(row)
+// const db = sqlite3("/fonts/ffxiv_database.sqlite3")
+
+export function getItem() {
+	const __filename = fileURLToPath(import.meta.url);
+	const __dirname = dirname(__filename);
+
+	console.log("DONDO");
+	console.log(__filename);
+	// var row = db.prepare('SELECT * FROM item WHERE id = ?').get(1)
+	// console.log(row)
 }
