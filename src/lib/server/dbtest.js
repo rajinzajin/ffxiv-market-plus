da3af8079f71c8da775de2f09f0791ae7c270bbd
rajinzajin <dorgclabs@gmail.com>
2023-04-17 09:27:51 +0800

    testing sqlite

---
+++ src/lib/server/dbtest.js
@@ -0,0 +1,8 @@
+import sqlite3 from "better-sqlite3"
+
+const db = sqlite3("src/database/ffxiv_database.sqlite3")
+
+export function getItem(){
+    var row = db.prepare('SELECT * FROM item WHERE id = ?').get(1)
+    console.log(row)
+}
--- svelte.config.js
+++ svelte.config.js
@@ -12,6 +12,9 @@ const config = {
 		// csrf: {
 		// 	checkOrigin: true,
 		// },
+		// alias:{
+		// 	'database' : "src/database"
+		// }
 	},
 	preprocess: vitePreprocess(),
 };
