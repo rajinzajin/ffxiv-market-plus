7b35216d892157ff6ad1e64274294ad98fd0593e
rajinzajin <dorgclabs@gmail.com>
2023-04-18 23:27:43 +0800

    use field selection in itemdb

--- prisma/item_db.ts
+++ prisma/item_db.ts
@@ -3,6 +3,9 @@ import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
 export async function getItem(_id) {
-	const data = await prisma.item.findFirst({ where: { id: parseInt(_id) } });
-    return data
+	const data = await prisma.item.findFirst({
+		where: { id: parseInt(_id) },
+		select: { id: true, Name: true, Description: true },
+	});
+	return data;
 }
