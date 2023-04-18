199aa33a6ec551d85294d55f656f441939bd5b04
rajinzajin <dorgclabs@gmail.com>
2023-04-18 11:51:32 +0800

    add card loading for item image

--- src/routes/market/[dc]/[item_id]/+page.svelte
+++ src/routes/market/[dc]/[item_id]/+page.svelte
@@ -2,17 +2,32 @@
 	import LhPriceCard from "../../../../components/LHPriceCard.svelte";
 	import { getItemImageUrl } from "../../../../utils/item_utils.js";
 	import ItemSearchBar from "../../../../components/ItemSearchBar.svelte";
-	import { onMount } from "svelte";
+	import { afterUpdate, beforeUpdate, onMount } from "svelte";
 	import { main_dc } from "../../../../stores/dc_world_stores";
+	import CardLoading from "../../../../components/CardLoading.svelte";
 	export let data;
+
+	let itemLoading;
+
+	$: {
+		if (data.item_detail != null) {
+			itemLoading = false;
+		}
+	}
+
+	function onSearchItemSelect(selected_item_id) {
+		if (data.item_detail.id != selected_item_id) {
+			itemLoading = true;
+		}
+	}
 
 	onMount(() => {
 		var unsubscribe_main_dc = main_dc.subscribe((value) => {
 			if (value != null) {
 				console.log(`${value.name} selected`);
-				
 			}
 		});
+
 		return () => {
 			unsubscribe_main_dc();
 		};
@@ -24,18 +39,23 @@
 		class="h-100 py-2 mb-6 px-5 w-96 max-w-full items-center justify-center rounded-2xl bg-item"
 	>
 		<div class="w-full mb-4">
-			<ItemSearchBar />
+			<ItemSearchBar on_select_item_callback={onSearchItemSelect} />
 		</div>
 	</div>
 
 	<div class="flex">
-		<img class="h-20" src={getItemImageUrl(data.item_id)} alt={data.name} />
+		<div class="relative">
+			<CardLoading show={itemLoading} />
+			<img class="h-20" src={getItemImageUrl(data.item_id)} alt={data.name} />
+		</div>
 		<div class="ml-5 font-display">
 			<h1 class="text-white text-4xl font-black my-auto">
 				{data.name}
 			</h1>
 			<h1 class="text-gray-400 text-lg font-black my-auto">
-				{ data.item_detail.Description !== null ? data.item_detail.Description : ""}
+				{data.item_detail.Description !== null
+					? data.item_detail.Description
+					: ""}
 			</h1>
 		</div>
 	</div>
