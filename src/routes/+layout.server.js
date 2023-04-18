8507545b845349822121f77627f8fc93ebcb5355
rajinzajin <dorgclabs@gmail.com>
2023-04-18 14:33:53 +0800

    removed unused code

--- src/routes/+layout.server.js
+++ src/routes/+layout.server.js
@@ -1,12 +1,9 @@
-import { initCache } from "../cache";
 import { getArrayFromCSV } from "../utils/csv_functions";
 import { getDataCenters } from "../utils/data_center_function";
 import { getWorlds } from "../utils/world_function";
 import marketable_items from "../data/marketable_items.json" assert { type: "json" };
-import {getItem} from "../../prisma/item_db"
 /** @type {import('./$types').LayoutServerLoad} */
 export async function load() {
-	await initCache();
 	
 	var data_centers = await getDataCenters();
 	var worlds = await getWorlds();
