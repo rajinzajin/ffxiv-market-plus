3ae7686947245d9f17409dd792e4957f78063d96
rajinzajin <dorgclabs@gmail.com>
2023-04-18 15:02:57 +0800

    add backdrop blur to cardloading

--- src/components/CardLoading.svelte
+++ src/components/CardLoading.svelte
@@ -5,6 +5,6 @@
 </script>
 
 <div class="{show_class} absolute w-full h-full">
-	<div class="absolute rounded-lg w-full h-full bg-opacity-50 bg-black" />
+	<div class="absolute rounded-lg w-full h-full bg-opacity-25 bg-black backdrop-blur-sm" />
 	<span class="absolute loader w-9 h-9 m-auto top-0 left-0 right-0 bottom-0" />
 </div>
