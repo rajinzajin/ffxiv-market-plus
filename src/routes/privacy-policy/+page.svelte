83b26ebfc17681059175c65c694aee8afdd1b9b6
rajinzajin <dorgclabs@gmail.com>
2023-04-19 16:03:00 +0800

    update privacy policy

--- src/routes/privacy-policy/+page.svelte
+++ src/routes/privacy-policy/+page.svelte
@@ -6,5 +6,37 @@
 
 <div class="text-white mt-10">
 	<h1 class="text-5xl font-black font-display">Privacy Policy</h1>
-	<p class="font-body" />
+
+	<div class="grid font-body grid-cols-12 gap-0 text-xl">
+		<div class="col-span-8">
+			<br />
+			&emsp;&emsp;At XIV Market+, we value your privacy and are committed to protecting
+			your personal information. This Privacy Policy outlines how we collect, use,
+			and safeguard your data when you use our web app.
+			<br />
+			<br />
+			<h1 class="text-2xl font-black">Information We Collect</h1>
+			<br />
+			&emsp;&emsp;XIV Market+ does not collect any personal data or personally identifiable
+			information. We do not require you to provide any personal information to use
+			our web app.
+			<br />
+			<br />
+			<h1 class="text-2xl font-black">Cookies</h1>
+			<br />
+			&emsp;&emsp;XIV Market+ does not use any cookies or tracking technologies to
+			collect information about your browsing activities. 
+			<br />
+			<br />
+			<h1 class="text-2xl font-black">Changes to Privacy Policy</h1>
+			<br />
+			&emsp;&emsp;We may update this Privacy Policy from time to time. Any changes will
+			be posted on this page, and the updated Privacy Policy will be effective as
+			of the date of posting.
+			<br />
+			<br />
+			<br />
+			<span class="text-gray-400">Last updated: April 19, 2023</span>
+		</div>
+	</div>
 </div>
