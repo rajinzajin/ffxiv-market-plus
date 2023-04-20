574b60d275ffa57281c01a1e6ccfd6a392999dad
rajinzajin <dorgclabs@gmail.com>
2023-04-20 13:27:10 +0800

    removed unused import

--- src/routes/+layout.server.js
+++ src/routes/+layout.server.js
@@ -2,7 +2,6 @@ import { getArrayFromCSV } from "../utils/csv_functions";
 import { getDataCenters } from "../utils/data_center_function";
 import { getWorlds } from "../utils/world_function";
 import marketable_items from "../data/marketable_items.json" assert { type: "json" };
-import { convertToArray } from "../utils/item_utils";
 
 /** @type {import('./$types').LayoutServerLoad} */
 export async function load() {
