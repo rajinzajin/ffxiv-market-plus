8befafdaf279d5cecd44118a42a46f8740b8b708
rajinzajin <dorgclabs@gmail.com>
2023-04-21 09:23:07 +0800

    added NQ and HQ button to table

--- src/components/MarketTable.svelte
+++ src/components/MarketTable.svelte
@@ -8,63 +8,98 @@
 
 	export let listings = [];
 
-	let worlds_listing = [];
+	const selectedStyle =
+		"text-white focus:ring-4 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 bg-blue-600 hover:bg-blue-700 focus:outline-none focus:ring-blue-800";
+	const unSelectedStyle =
+		"py-2.5 px-5 mr-2 mb-2 text-sm font-medium focus:outline-none rounded-lg border focus:z-10 focus:ring-4 focus:ring-gray-700 bg-gray-800 text-gray-400 border-gray-600 hover:text-white hover:bg-gray-700";
 
-	$: worlds_listing = updateData(listings);
+	let selectedPrices = [];
+	let worlds_price;
+	let isHqTable = false;
+
+	$: worlds_price = updateData(listings);
+	$: selectedPrices = isHqTable ? worlds_price.hq : worlds_price.nq;
 
 	function updateData(_listings) {
-		var result = [];
-		if (_listings.length > 0) {
-			var worlds = getDataCenter(get(main_dc)).worlds ?? [];
-			worlds.forEach((world_id) => {
-				var world = getWorld(world_id);
-				var worlds_listings = getWorldListing(world.name);
-				if (worlds_listings.length > 0) {
-					result.push(getLowestPriceItem(worlds_listings));
-				}
-			});
-			result.sort((a, b) => a.pricePerUnit - b.pricePerUnit);
+		var price_list = { nq: [], hq: [] };
+		if (_listings.length <= 0) {
+			return price_list;
 		}
-		return result;
+
+		var worlds = getDataCenter(get(main_dc)).worlds ?? [];
+		worlds.forEach((world_id) => {
+			var world = getWorld(world_id);
+			var nq = getWorldListing(world.name, false);
+			var hq = getWorldListing(world.name, true);
+			if (nq.length > 0) {
+				price_list.nq.push(getLowestPriceItem(nq));
+			}
+			if (hq.length > 0) {
+				price_list.hq.push(getLowestPriceItem(hq));
+			}
+		});
+		price_list.nq.sort((a, b) => a.pricePerUnit - b.pricePerUnit);
+		price_list.hq.sort((a, b) => a.pricePerUnit - b.pricePerUnit);
+		return price_list;
 	}
 
-	function getWorldListing(worldName) {
-		return listings.filter((listing) => listing.worldName === worldName);
+	function getWorldListing(worldName, hq) {
+		return listings.filter(
+			(listing) => listing.worldName === worldName && listing.hq === hq
+		);
 	}
 </script>
 
 <div class="relative overflow-x-auto shadow-md sm:rounded-lg">
-	<table class="w-full font-display text-sm text-left text-gray-300">
-		<thead
-			class="bg-item text-sm font-body text-gray-700 uppercase dark:text-gray-400"
-		>
-			<tr>
-				<th scope="col" class="px-6 py-3"> World </th>
-				<th scope="col" class="px-6 py-3"> Price </th>
-				<th scope="col" class="px-6 py-3"> Quantity </th>
-				<th scope="col" class="px-6 py-3"> Quality </th>
-			</tr>
-		</thead>
-		<tbody>
-			{#each worlds_listing as world_listing}
-				<tr
-					class="bg-primary text-base border-b border-gray-700 hover:bg-gray-600"
-				>
-					<th
-						scope="row"
-						class="px-6 py-4 font-medium whitespace-nowrap text-white"
-					>
-						{world_listing.worldName}
-					</th>
-					<td class="px-6 py-4">
-						{formatNumberToGilString(world_listing.pricePerUnit)}
-					</td>
-					<td class="px-6 py-4"> {world_listing.quantity} </td>
-					<td class="px-6 py-4">
-						{world_listing.hq === true ? "High" : "Normal"}
-					</td>
+	<button
+		on:click={() => (isHqTable = false)}
+		type="button"
+		class={isHqTable ? unSelectedStyle : selectedStyle}>Normal Quality</button
+	>
+	<button
+		on:click={() => (isHqTable = true)}
+		type="button"
+		class={isHqTable ? selectedStyle : unSelectedStyle}>High Quality</button
+	>
+	{#if selectedPrices.length > 0}
+		<table class="w-full font-display text-sm text-left text-gray-300">
+			<thead
+				class="bg-item text-sm font-body text-gray-700 uppercase dark:text-gray-400"
+			>
+				<tr>
+					<th scope="col" class="px-6 py-3"> World </th>
+					<th scope="col" class="px-6 py-3"> Price </th>
+					<th scope="col" class="px-6 py-3"> Quantity </th>
+					<th scope="col" class="px-6 py-3"> Quality </th>
 				</tr>
-			{/each}
-		</tbody>
-	</table>
+			</thead>
+			<tbody>
+				{#each selectedPrices as item}
+					<tr
+						class="bg-primary text-base border-b border-gray-700 hover:bg-gray-600"
+					>
+						<th
+							scope="row"
+							class="px-6 py-4 font-medium whitespace-nowrap text-white"
+						>
+							{item.worldName}
+						</th>
+						<td class="px-6 py-4">
+							{formatNumberToGilString(item.pricePerUnit)}
+						</td>
+						<td class="px-6 py-4"> {item.quantity} </td>
+						<td class="px-6 py-4">
+							{item.hq === true ? "High" : "Normal"}
+						</td>
+					</tr>
+				{/each}
+			</tbody>
+		</table>
+	{:else}
+		<div class="bg-item text-sm font-body uppercase text-gray-400 py-3">
+			<h1 class="font-body text-white font-semibold text-base text-center">
+				No Item Found.
+			</h1>
+		</div>
+	{/if}
 </div>
