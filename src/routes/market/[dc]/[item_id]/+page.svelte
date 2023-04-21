55ba61342df97e977ba9b68fce3431ac6b8f374c
rajinzajin <dorgclabs@gmail.com>
2023-04-21 11:23:20 +0800

    material icon as font face

--- src/routes/market/[dc]/[item_id]/+page.svelte
+++ src/routes/market/[dc]/[item_id]/+page.svelte
@@ -101,7 +101,7 @@
 						on:keyup={() => {}}
 						class="{marketLoading
 							? 'animate-spin'
-							: ''} text-white select-none cursor-pointer text-3xl font-black material-symbols-outlined"
+							: ''} text-white select-none cursor-pointer text-3xl font-black font-material_icon"
 					>
 						refresh
 					</span>
